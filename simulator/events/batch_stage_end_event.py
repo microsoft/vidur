1381736c5c90287119fa0afdcf83c13f59483129
Panwar, Ashish <ashishpanwar@microsoft.com>
2023-11-07 11:33:03 +0530

    minor
    Signed-off-by: Panwar, Ashish <ashishpanwar@microsoft.com>

--- simulator/events/batch_stage_end_event.py
+++ simulator/events/batch_stage_end_event.py
@@ -8,12 +8,6 @@ from simulator.plotting import MetricsStore
 from simulator.scheduler import BaseGlobalScheduler
 from simulator.types import EventType
 
-from simulator.events.batch_end_event import BatchEndEvent
-from simulator.events.batch_stage_arrival_event import BatchStageArrivalEvent
-from simulator.events.replica_stage_schedule_event import (
-    ReplicaStageScheduleEvent,
-)
-
 logger = logging.getLogger(__name__)
 
 
@@ -43,7 +37,11 @@ class BatchStageEndEvent(BaseEvent):
     def handle_event(
         self, scheduler: BaseGlobalScheduler, metrics_store: MetricsStore
     ) -> List[BaseEvent]:
-
+        from simulator.events.batch_end_event import BatchEndEvent
+        from simulator.events.batch_stage_arrival_event import BatchStageArrivalEvent
+        from simulator.events.replica_stage_schedule_event import (
+            ReplicaStageScheduleEvent,
+        )
         scheduler.get_replica_stage_scheduler(
             self._replica_id, self._stage_id
         ).on_stage_end()
