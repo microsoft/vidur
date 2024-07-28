48475abf7685c39ce1cfc32c35d1918fdb4f073c
Anmol Agarwal <anmol.agarwalcp810@gmail.com>
2024-07-28 00:45:54 -0400

    ip address

--- vidur/config_optimizer/config_explorer/ray_utils.py
+++ vidur/config_optimizer/config_explorer/ray_utils.py
@@ -7,7 +7,16 @@ import ray
 
 
 def get_ip() -> str:
-    return socket.gethostbyname(socket.gethostname())
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(("10.254.254.254", 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
 
 
 def get_nodes() -> list[str]:
