a28d390816aeb9c12b8881ee8b2c2924585a9a98
Nitin Kedia <nitinkedia7@gmail.com>
2024-08-16 08:10:35 +0000

    Use matching column names with trace_request_length_generator for interoperatiblity

--- vidur/request_generator/trace_replay_request_generator.py
+++ vidur/request_generator/trace_replay_request_generator.py
@@ -21,59 +21,59 @@ class TraceReplayRequestGenerator(BaseRequestGenerator):
 
         # load into a pd dataframe
         self.trace_df = pd.read_csv(config.trace_file)
-        # restrict trace_df to be a subset of rows that have the same date
-        self.trace_df = self.trace_df[self.trace_df["Date"] == config.date]
 
         # scale prefill and decode tokens
-        self.trace_df["PromptTokenCount"] = (
-            self.trace_df["PromptTokenCount"] * config.prefill_scale_factor
+        self.trace_df["num_prefill_tokens"] = (
+            self.trace_df["num_prefill_tokens"] * config.prefill_scale_factor
         )
-        self.trace_df["CompletionTokenCount"] = (
-            self.trace_df["CompletionTokenCount"] * config.decode_scale_factor
+        self.trace_df["num_decode_tokens"] = (
+            self.trace_df["num_decode_tokens"] * config.decode_scale_factor
         )
 
         # make sure all the prefill and decode counts are integers
-        self.trace_df["PromptTokenCount"] = self.trace_df["PromptTokenCount"].astype(
-            int
-        )
-        self.trace_df["CompletionTokenCount"] = self.trace_df[
-            "CompletionTokenCount"
+        self.trace_df["num_prefill_tokens"] = self.trace_df[
+            "num_prefill_tokens"
+        ].astype(int)
+        self.trace_df["num_decode_tokens"] = self.trace_df[
+            "num_decode_tokens"
         ].astype(int)
 
         # make sure that there is at least one prefill and decode token
-        self.trace_df["PromptTokenCount"] = self.trace_df["PromptTokenCount"].clip(
+        self.trace_df["num_prefill_tokens"] = self.trace_df[
+            "num_prefill_tokens"
+        ].clip(lower=1)
+        self.trace_df["num_decode_tokens"] = self.trace_df["num_decode_tokens"].clip(
             lower=1
         )
-        self.trace_df["CompletionTokenCount"] = self.trace_df[
-            "CompletionTokenCount"
-        ].clip(lower=1)
 
         # make sure the total does not exceed the max tokens, adjust the prefill tokens if needed
         total_tokens = (
-            self.trace_df["PromptTokenCount"] + self.trace_df["CompletionTokenCount"]
+            self.trace_df["num_prefill_tokens"] + self.trace_df["num_decode_tokens"]
         )
         diff_tokens = total_tokens - config.max_tokens
         diff_tokens = diff_tokens.clip(lower=0)
-        self.trace_df["PromptTokenCount"] = (
-            self.trace_df["PromptTokenCount"] - diff_tokens
+        self.trace_df["num_prefill_tokens"] = (
+            self.trace_df["num_prefill_tokens"] - diff_tokens
         )
 
         assert all(
-            self.trace_df["PromptTokenCount"] + self.trace_df["CompletionTokenCount"]
+            self.trace_df["num_prefill_tokens"] + self.trace_df["num_decode_tokens"]
             <= config.max_tokens
         )
 
         # rescale the time to change QPS
-        self.trace_df["Time"] = self.trace_df["Time"] * config.time_scale_factor
-
-        # compute pd ratio and log the 25, 50, 75, 90, 95, 99 percentiles
-        pd_ratio = (
-            self.trace_df["PromptTokenCount"] / self.trace_df["CompletionTokenCount"]
+        self.trace_df["arrived_at"] = (
+            self.trace_df["arrived_at"] * config.time_scale_factor
         )
+
         logger.info(
             f"Loaded trace file {config.trace_file} with {len(self.trace_df)} requests"
         )
-        logger.info(
+        # compute pd ratio and log the 25, 50, 75, 90, 95, 99 percentiles
+        pd_ratio = (
+            self.trace_df["num_prefill_tokens"] / self.trace_df["num_decode_tokens"]
+        )
+        logger.debug(
             f"Prompt/decode token ratio stats\n:{pd_ratio.describe(percentiles=[0.25, 0.5, 0.75, 0.9, 0.95, 0.99])}"
         )
 
@@ -82,9 +82,9 @@ class TraceReplayRequestGenerator(BaseRequestGenerator):
 
         for _, row in self.trace_df.iterrows():
             request = Request(
-                arrived_at=row["Time"],
-                num_prefill_tokens=row["PromptTokenCount"],
-                num_decode_tokens=row["CompletionTokenCount"],
+                arrived_at=row["arrived_at"],
+                num_prefill_tokens=row["num_prefill_tokens"],
+                num_decode_tokens=row["num_decode_tokens"],
             )
 
             requests.append(request)
